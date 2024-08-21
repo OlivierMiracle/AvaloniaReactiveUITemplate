8d5bb1bd636fb3281b6fe7842695d68d67985765
OliverMiracle <adam.cisz@pm.me>
2024-08-21 08:00:50 +0200

    Add factory method and private constructor to the MainViewModel

--- Application/MainViewModel.cs
+++ Application/MainViewModel.cs
@@ -5,11 +5,21 @@ namespace Application;
 
 public sealed class MainViewModel  : ReactiveObject
 {
+    public static MainViewModel Create(Main model, IServiceProvider services)
+    {
+        return new MainViewModel(model);
+    }
+    
     private Main _main;
 
     public string MainText
     {
         get => _main._mainText;
         set => this.RaiseAndSetIfChanged(ref _main._mainText, value);
+    }
+    
+    private MainViewModel(Main main)
+    {
+        _main = main;
     }
 }
